4974bc9d8534c63ab67d86b2c39095601109141e
EngZhyar <147944202+EngZhyar@users.noreply.github.com>
2026-03-29 00:18:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
 const data = [
-{
-name:"Calibration Modern",
-owner:"Ahmad Nasraddin",
-phone1:"+964 782 557 6825",
-phone2:" ",
-location:" https://maps.app.goo.gl/uDd14ByFswCP9CpcA?g_st=iw",
-status:"active"
-},
-{
-name:"Haware Hawler 2 (Camp)",
-owner:"Nawar IT manager",
-phone1:"+964 783 577 9253",
-phone2:" ",
-location:"https://maps.app.goo.gl/8MhFnncGykc6kaEh6",
-status:"active"
-},
-{
-name:"Oscar hotel",
-owner:"",
-phone1:"+964 750 420 1089",
-phone2:" ",
-location:"https://maps.app.goo.gl/ACkEhvGuMdSwQXY28",
-status:"active"
-},
-{
-name:"Vote 2 hotel",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/SE7BHmV3u33spxHJ8",
-status:"active"
-},
+
+//A
 {
 name:"Ararat Hotel",
 owner:"",
@@ -39,6 +9,7 @@ phone2:" ",
 location:"https://maps.app.goo.gl/sp64ry1Jx9NvwSGb9",
 status:"active"
 },
+
 {
 name:"AD Hotel 1",
 owner:"",
@@ -55,38 +26,7 @@ phone2:" ",
 location:"https://maps.app.goo.gl/JoA1HgcsHmDMQSgM7",
 status:"active"
 },
-{
-name:"Blue Rose Motel",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/zsBsVKvysybxeesC6",
-status:"active"
-},
-{
-name:"Dwnay Hotel",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/GYWpKJd4oooZqbSt7",
-status:"active"
-},
-{
-name:"Qalb Almulk",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/WzGC2TkVg1R3MxT58",
-status:"active"
-},
-{
-name:"Ice Cream Scoop",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/6fpF9GBEAn14ZTxHA",
-status:"active"
-},
+
 {
 name:"Amas Hypermarket",
 owner:"",
@@ -95,110 +35,11 @@ phone2:" ",
 location:"https://maps.app.goo.gl/ygJKRKMCAQohKGY7A",
 status:"active"
 },
-{
-name:"Khalat",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/C3zcsRdjhUUkLP137",
-status:"active"
-},
-{
-name:"Zarawa Company",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/ngXUh8XCRyYnDDUV8?g_st=aw",
-status:"active"
-},
-{
-name:"Pop Cafe",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/cX3T63m87cdsqY2L9",
-status:"active"
-},
-{
-name:"Rodina Hotel",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/NtVkjxY92DHb3P3A6",
-status:"active"
-},
-{
-name:"Golden Night",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/4XVrBgjc2637DcBFA",
-status:"active"
-},
-{
-name:"Samad Resturant",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/DKDEhn7tkrMacXgN9",
-status:"active"
-},
-{
-name:"Koshki Rashid",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/RgH2wmHgNNJQ8qPv7",
-status:"active"
-},
-{
-name:"Rihat Hotel",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/eKqh4RGZbwXiU7sP7",
-status:"deactive  24/3/2026"
-},
-{
-name:"Skye hamrin",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/Xn8pExEzvsiEV8fe7",
-status:"active"
-},
-{
-name:"Omer Tank",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/Y8EnuuMNeEXjfxcC6",
-status:"active"
-},
-{
-name:"Qalay Iskan",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/PNLYTtY9xXCKFxn66",
-status:"active"
-},
-{
-name:"Sherzad",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/XWDcunjxiSQ2cHfw6",
-status:"active"
-},
-{
-name:"Washnton Hotel",
-owner:"Dlovan",
-phone1:"+964 750 422 0123",
-phone2:" ",
-location:"https://maps.app.goo.gl/PXN3yPVfCVr5suuZ9",
-status:"active"
-},
+
+
+//B
+
+,
 {
 name:"Bahare Kar Shorsh",
 owner:"",
@@ -207,38 +48,30 @@ phone2:" ",
 location:"https://maps.app.goo.gl/qQJegrF4VCmQfgPA8",
 status:"active"
 },
+
+//b
+
 {
-name:"Najm Iskan Cafe",
-owner:"bhrnk",
-phone1:"+964 770 129 9532",
-phone2:" ",
-location:"https://maps.app.goo.gl/Sp9aQdvNndkBqWfq5",
-status:"active"
-},
-{
-name:"Male Gashtearan",
-owner:"",
-phone1:"+964 750 853 3333",
-phone2:" ",
-location:"https://maps.app.goo.gl/RsHBJRckY6AXTA4q7",
-status:"active"
-},
-{
-name:"Halqa Nadafa",
+name:"Blue Rose Motel",
 owner:"",
 phone1:"",
 phone2:" ",
-location:"https://maps.app.goo.gl/8TRaeD4HyBpd7BL3A",
+location:"https://maps.app.goo.gl/zsBsVKvysybxeesC6",
 status:"active"
 },
+
+//c
+
 {
-name:"Mam Concret",
-owner:"",
-phone1:"",
+name:"Calibration Modern",
+owner:"Ahmad Nasraddin",
+phone1:"+964 782 557 6825",
 phone2:" ",
-location:"https://maps.app.goo.gl/AfmANnU6cq18mpBA6",
+location:" https://maps.app.goo.gl/uDd14ByFswCP9CpcA?g_st=iw",
 status:"active"
 },
+
+//d
 {
 name:"Detail Company",
 owner:"",
@@ -246,26 +79,13 @@ phone1:"",
 phone2:" ",
 location:"https://maps.app.goo.gl/dQiPGXa8sMB3fsmE8",
 status:"active"
-},
-{
-name:"King school wazeran",
-owner:"Ziad Surchi",
-phone1:"+964 750 363 3454",
-phone2:" ",
-location:"https://www.google.com/maps?q=36.20460510253906,43.98479080200195&z=17&hl=en",
-status:"active"
-},
-{
-name:"King School",
-owner:"",
-phone1:"",
-phone2:" ",
-location:"https://maps.app.goo.gl/mwWHMNqsUhM5Jauj6",
-status:"active"
-},
+}
+,
+  
+  
+//k
 
-
-{
+  {
 name:"Kinam Hotel",
 owner:"",
 phone1:"",
@@ -275,28 +95,81 @@ status:"active"
 },
 
 
-  
 {
-name:"Mam Company",
+name:"Koshki Rashid",
 owner:"",
 phone1:"",
 phone2:" ",
-location:"https://maps.app.goo.gl/uHTdRzVyy2TF7fD46",
+location:"https://maps.app.goo.gl/RgH2wmHgNNJQ8qPv7",
+status:"active"
+},
+
+{
+name:"King School",
+owner:"",
+phone1:"",
+phone2:" ",
+location:"https://maps.app.goo.gl/mwWHMNqsUhM5Jauj6",
+status:"active"
+},
+
+{
+name:"King school wazeran",
+owner:"Ziad Surchi",
+phone1:"+964 750 363 3454",
+phone2:" ",
+location:"https://www.google.com/maps?q=36.20460510253906,43.98479080200195&z=17&hl=en",
+status:"active"
+}
+
+,
+
+{
+name:"Khalat",
+owner:"",
+phone1:"",
+phone2:" ",
+location:"https://maps.app.goo.gl/C3zcsRdjhUUkLP137",
 status:"active"
 },
 
 
+  
+
+//m
+
+  ,
+{
+name:"Male Gashtearan",
+owner:"",
+phone1:"+964 750 853 3333",
+phone2:" ",
+location:"https://maps.app.goo.gl/RsHBJRckY6AXTA4q7",
+status:"active"
+},
+
 
 {
-name:"Hanzo Hotel",
-owner:"Rzgar",
-phone1:"+964 750 805 4542",
+name:"Mam Concret",
+owner:"",
+phone1:"",
 phone2:" ",
-location:"https://maps.app.goo.gl/qdsNC6PzKCPiPDjKA",
+location:"https://maps.app.goo.gl/AfmANnU6cq18mpBA6",
+status:"active"
+},
+
+
+  
+//z
+,
+{
+name:"Zarawa Company",
+owner:"",
+phone1:"",
+phone2:" ",
+location:"https://maps.app.goo.gl/ngXUh8XCRyYnDDUV8?g_st=aw",
 status:"active"
 }
-
-
   
 ];
 
